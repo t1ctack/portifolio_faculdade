4ff2841b5027719dc932f257a9ca7d0350ab0042
Luis Felipe <louid090louiddrews@gmail.com>
2025-04-18 12:19:08 -0300

    criando a tabela aluno

--- modelo_fisico.sql
+++ modelo_fisico.sql
@@ -1,2 +1,11 @@
 create database faculdade;
 use faculdade;
+
+create table tbl_aluno 
+(
+	id_aluno int primary key not null auto_increment,
+    data_nascimento date not null,
+    nome varchar(100) not null
+    
+);
+
